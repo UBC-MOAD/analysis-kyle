a2e1f59bc0fce67c318d82ba61c1c45afaa908a6
Yingkai Sha (Kyle) <yingkai@eos.ubc.ca>
2015-09-30 11:56:07 +0800

    New simulation results updated, new d18O forcing work updated.

---
+++ notebooks/_data/interp_d18O.m
@@ -0,0 +1,28 @@
+clear all
+close all
+load ORCA2_deptht
+deptht=double(deptht);
+d18O=ncread('calculated_d18O_v1_1.nc', 'd18o');
+depth=double(ncread('calculated_d18O_v1_1.nc', 'depth'));
+test = permute(d18O, [2, 1, 3]);
+d18O=double(test);
+d18O(d18O<-1e+10)=nan;
+clear test
+x=ncread('calculated_d18O_v1_1.nc', 'lon');
+y=ncread('calculated_d18O_v1_1.nc', 'lat');
+[lon, lat]=meshgrid(x, y);
+clear x y
+for i=1:33
+    layer=inpaint_nans(d18O(:, :, i), 3);
+    layer(layer<=-7)=-7.0;
+    layer(layer>5)=5;
+    d18O(:, :, i)=layer;
+end
+d18O_ORCA2=zeros([size(d18O, 1), size(d18O, 2), 31]);
+for i=1:size(d18O, 1)
+    for j=1:size(d18O, 2)
+        d18O_ORCA2(i, j, :)=interp1(depth, squeeze(d18O(i, j, :)), deptht, 'linear');
+    end
+end
+        
+%test = interp2(lon, lat, d18O(:, :, 1), lon, lat, 'nearest');
