e03bd6cb35c166464c82cd314d9aa928e6419f4c
Yingkai Sha (Kyle) <yingkai@eos.ubc.ca>
2016-01-27 15:48:01 -0800

    Checking file dependences of the old notebooks

--- notebooks/_script/Kriging_Interp_Ba.py
+++ notebooks/_script/Kriging_Interp_Ba.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '_libs/')
+sys.path.insert(0, '../_libs/')
 import glob
 #import pyproj
 import scipy.io
@@ -30,7 +30,7 @@ def remove_nan(x, y, data):
     y_temp = y[~np.isnan(data)]
     return x_temp, y_temp, data_temp
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/GIPY11_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/GIPY11_Ba.mat')
 GIPY_lons = MAT['Ba_records'][:, 0]
 GIPY_lats = MAT['Ba_records'][:, 1]
 GIPY_deps = MAT['Ba_records'][:, 2]
@@ -38,34 +38,34 @@ GIPY_Ba   = MAT['Ba_records'][:, 3]
 #
 GIPY_lons[GIPY_lons>180]=GIPY_lons[GIPY_lons>180]-360
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/BGEP_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/BGEP_Ba.mat')
 x03 = MAT['Ba2003'][:, 1]; y03 = MAT['Ba2003'][:, 0]; z03 = MAT['Ba2003'][:, 2]; Ba03 = MAT['Ba2003'][:, 3]
 x04 = MAT['Ba2004'][:, 1]; y04 = MAT['Ba2004'][:, 0]; z04 = MAT['Ba2004'][:, 2]; Ba04 = MAT['Ba2004'][:, 3]
 x04[Ba04<0]=np.nan; y04[Ba04<0]=np.nan; z04[Ba04<0]=np.nan; Ba04[Ba04<0]=np.nan
 x05 = MAT['Ba2005'][:, 1]; y05 = MAT['Ba2005'][:, 0]; z05 = MAT['Ba2005'][:, 2]; Ba05 = MAT['Ba2005'][:, 3]
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/HLY_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/HLY_Ba.mat')
 xH = np.squeeze(MAT['lon'][:]); yH = np.squeeze(MAT['lat'][:]); zH = np.squeeze(MAT['dep'][:]); BaH = np.squeeze(MAT['Ba'][:])
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/CBL_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/CBL_Ba.mat')
 xC = np.squeeze(MAT['lon'][:]); yC = np.squeeze(MAT['lat'][:]); zC = np.squeeze(MAT['dep'][:]); BaC = np.squeeze(MAT['Ba'][:])
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/ARK09_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/ARK09_Ba.mat')
 xA9 = np.squeeze(MAT['lon'][:]); yA9 = np.squeeze(MAT['lat'][:]); zA9 = np.squeeze(MAT['dep'][:]); BaA9 = np.squeeze(MAT['Ba'][:])
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/ARK14_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/ARK14_Ba.mat')
 xA14 = np.squeeze(MAT['lon'][:]); yA14 = np.squeeze(MAT['lat'][:]); zA14 = np.squeeze(MAT['dep'][:]); BaA14 = np.squeeze(MAT['Ba'][:])
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/NPEO_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/NPEO_Ba.mat')
 xNPEO = np.squeeze(MAT['lon'][:]); yNPEO = np.squeeze(MAT['lat'][:]); zNPEO = np.squeeze(MAT['dep'][:]); BaNPEO = np.squeeze(MAT['Ba'][:])
 
-MAT = scipy.io.loadmat('../../Arctic-obs/MATLAB files/ARC_Ba.mat')
+MAT = scipy.io.loadmat('../../../Arctic-obs/MATLAB files/ARC_Ba.mat')
 xARC = np.squeeze(MAT['lon'][:]); yARC = np.squeeze(MAT['lat'][:]); zARC = np.squeeze(MAT['dep'][:]); BaARC = np.squeeze(MAT['Ba'][:])
 
 #nc_name=glob.glob('/ocean/yingkai/GEOTRACES/NEMO-CODE/NEMOGCM/CONFIG/OFF_TEST/EXP00/EXP01_1m_00010101_00041001_ptrc_T.nc')
 #nc_obj=nc.Dataset(nc_name[0])
 #deptht = nc_obj.variables['deptht'][:]
-ANHA4_MAT=scipy.io.loadmat('_data/Exchange/coord_ANHA4.mat')
+ANHA4_MAT=scipy.io.loadmat('../_data/Exchange/coord_ANHA4.mat')
 deptht = ANHA4_MAT['nav_lev'][:]
 
 BGEP_lons = np.hstack((x03, x04, x05))
@@ -188,19 +188,19 @@ for i in range(num_layer):
     #fig.savefig('Barium_layer_'+str(i)+'modified.pdf', dpi=400, orientation='portrait', papertype='a4', format='pdf',
     #        transparent=False, bbox_inches='tight', pad_inches=0)
 
-coordinate_name=glob.glob('/ocean/yingkai/GEOTRACES/FORCING/ORCA2_LIM_nemo_v3.4/coordinates*.nc')
-coordinate_obj=nc.Dataset(coordinate_name[0])
-nav_lon=coordinate_obj.variables['nav_lon'][:]
-nav_lat=coordinate_obj.variables['nav_lat'][:]
+#coordinate_name=glob.glob('/ocean/yingkai/GEOTRACES/FORCING/ORCA2_LIM_nemo_v3.4/coordinates*.nc')
+#coordinate_obj=nc.Dataset(coordinate_name[0])
+#nav_lon=coordinate_obj.variables['nav_lon'][:]
+#nav_lat=coordinate_obj.variables['nav_lat'][:]
 #
 lon_list = np.linspace(-180, 180, 360)
 lat_list = np.linspace(60, 90, 30)
 lonxy, latxy = np.meshgrid(lon_list, lat_list)
 
 Ba_ini_xy = np.empty([np.size(lonxy, 0), np.size(lonxy, 1), num_layer])
-Ba_ini_orca = np.empty([np.size(nav_lon, 0), np.size(nav_lon, 1), num_layer])
+#Ba_ini_orca = np.empty([np.size(nav_lon, 0), np.size(nav_lon, 1), num_layer])
 #hit = find_inland(nav_lon, nav_lat)
-hit = np.zeros(latxy.shape)
+#hit = np.zeros(latxy.shape)
 
 for i in range(num_layer):
     int_temp = reporj_NEMOgrid(gridx, gridy, Ba_ini[:, :, i], lonxy, latxy, method='linear')
@@ -215,79 +215,57 @@ for i in range(num_layer):
     
 Ba_ini_masked = np.ma.masked_where(np.isnan(Ba_ini_xy), Ba_ini_xy)
 
-plt.pcolor(lonxy, latxy, Ba_ini_masked[:, :, 0])
-ax = plt.gca(); ax.set_xlim(-180, 180), ax.set_ylim(60, 90)
+#plt.pcolor(lonxy, latxy, Ba_ini_masked[:, :, 0])
+#ax = plt.gca(); ax.set_xlim(-180, 180), ax.set_ylim(60, 90)
 
-ETOPO2_Arctic=scipy.io.loadmat('_libs/ETOPO2_Arctic.mat')
-lon_arctic=ETOPO2_Arctic['lon_arctic']
-lat_arctic=ETOPO2_Arctic['lat_arctic']
-topo_arctic=ETOPO2_Arctic['topo_arctic']
+#ETOPO2_Arctic=scipy.io.loadmat('_libs/ETOPO2_Arctic.mat')
+#lon_arctic=ETOPO2_Arctic['lon_arctic']
+#lat_arctic=ETOPO2_Arctic['lat_arctic']
+#topo_arctic=ETOPO2_Arctic['topo_arctic']
 
-# Adjust resolution
-res_unit=5
-lon_arctic=lon_arctic[0:-1:res_unit, 0:-1:res_unit]
-lat_arctic=lat_arctic[0:-1:res_unit, 0:-1:res_unit]
-topo_arctic=topo_arctic[0:-1:res_unit, 0:-1:res_unit]*-1
+## Adjust resolution
+#res_unit=5
+#lon_arctic=lon_arctic[0:-1:res_unit, 0:-1:res_unit]
+#lat_arctic=lat_arctic[0:-1:res_unit, 0:-1:res_unit]
+#topo_arctic=topo_arctic[0:-1:res_unit, 0:-1:res_unit]*-1
 
-clevs=[1000, 2000, 3000]
-for num in range(num_layer):
-    # Locations of CTDs
-    for j in keys:
-        x_cord[j], y_cord[j], _ = remove_nan(x_int[j], y_int[j], Ba_int[j][:, num])
-    # Figures
-    fig=plt.figure(figsize=(10, 10)); ax=plt.gca()
-    proj=Basemap(projection='npstere', resolution='l', boundinglat=55, lon_0=90, round=True, ax=ax)
-    proj.drawmeridians(np.arange(0, 360, 60), labels=[1, 1, 1, 1], fontsize=10, latmax=90,linewidth=0)
-    proj.fillcontinents(color=[0.5, 0.5, 0.5], lake_color=None)
-    proj.drawcoastlines(linewidth=1.5, color='k')
-    gridx, gridy = proj(lonxy, latxy)
-    topox, topoy = proj(lon_arctic, lat_arctic)
-    for j in keys:
-        x_cord[j], y_cord[j] = proj(x_cord[j], y_cord[j])
-    CS = proj.pcolor(gridx, gridy, Ba_ini_masked[:, :, num], vmin=30, vmax=100, cmap=plt.cm.gist_ncar_r)
-    CS2 = proj.contour(topox, topoy, topo_arctic, clevs, linestyles='-', linewidths=1, colors=('k',), alpha=0.75)
-    CS2.collections[0].set_label('1, 2, 3 km bathymetry')
-    proj.plot(x_cord['GIPY'] , y_cord['GIPY'] , 'kx', ms=6, mew=2.5, label='GIPY11 2007')
-    proj.plot(x_cord['BGEP'] , y_cord['BGEP'] , 'k^', ms=6, mew=0.5, label='BGEP 2003-2005')
-    proj.plot(x_cord['NPEO'] , y_cord['NPEO'] , 'k*', ms=9, mew=1.0, label='NPEO 2001-2005')
-    proj.plot(x_cord['HLY']  , y_cord['HLY']  , 'k+', ms=8, mew=2.5, label='HLY0301 2003')
-    proj.plot(x_cord['CBL']  , y_cord['CBL']  , 'k<', ms=6, mew=0.5, label='CBL 2002')
-    proj.plot(x_cord['ARK14'], y_cord['ARK14'], 'k>', ms=6, mew=0.5, label='ARKTISXIV-II 1998')
-    proj.plot(x_cord['ARC']  , y_cord['ARC']  , 'kv', ms=6, mew=0.5, label='ARCSS107-1993')
-    #proj.plot(gridx.T, gridy.T, 'k-', lw=0.25)
-    #proj.plot(gridx, gridy, 'k-', lw=0.25)
-    cbaxes = fig.add_axes([0.975, 0.15, 0.02, 0.5]) 
-    CBar = plt.colorbar(CS, ax=ax, cax=cbaxes) 
-    CBar.set_label('Interpolated diss.Ba con. ( 1E-6 mol/L )', fontsize=12)
-    CBar.ax.tick_params(axis='y', length=0)
-    LG = ax.legend(numpoints=1, bbox_to_anchor=(0.95, 1), loc=2, borderaxespad=0.); LG.draw_frame(False)
-    ax.set_title('Initial field in a regular lat/lon frame, depth='+str(deptht[num])+'m', fontsize=15, y=1.025)
-    fig.savefig('Initial_field'+str(num)+'.pdf', dpi=600, orientation='portrait', papertype='a4', format='pdf',
-                transparent=False, bbox_inches='tight', pad_inches=0)
+#clevs=[1000, 2000, 3000]
+#for num in range(num_layer):
+#    # Locations of CTDs
+#    for j in keys:
+#        x_cord[j], y_cord[j], _ = remove_nan(x_int[j], y_int[j], Ba_int[j][:, num])
+#    # Figures
+#    fig=plt.figure(figsize=(10, 10)); ax=plt.gca()
+#    proj=Basemap(projection='npstere', resolution='l', boundinglat=55, lon_0=90, round=True, ax=ax)
+#    proj.drawmeridians(np.arange(0, 360, 60), labels=[1, 1, 1, 1], fontsize=10, latmax=90,linewidth=0)
+#    proj.fillcontinents(color=[0.5, 0.5, 0.5], lake_color=None)
+#    proj.drawcoastlines(linewidth=1.5, color='k')
+#    gridx, gridy = proj(lonxy, latxy)
+#    topox, topoy = proj(lon_arctic, lat_arctic)
+#    for j in keys:
+#        x_cord[j], y_cord[j] = proj(x_cord[j], y_cord[j])
+#    CS = proj.pcolor(gridx, gridy, Ba_ini_masked[:, :, num], vmin=30, vmax=100, cmap=plt.cm.gist_ncar_r)
+#    CS2 = proj.contour(topox, topoy, topo_arctic, clevs, linestyles='-', linewidths=1, colors=('k',), alpha=0.75)
+#    CS2.collections[0].set_label('1, 2, 3 km bathymetry')
+#    proj.plot(x_cord['GIPY'] , y_cord['GIPY'] , 'kx', ms=6, mew=2.5, label='GIPY11 2007')
+#    proj.plot(x_cord['BGEP'] , y_cord['BGEP'] , 'k^', ms=6, mew=0.5, label='BGEP 2003-2005')
+#    proj.plot(x_cord['NPEO'] , y_cord['NPEO'] , 'k*', ms=9, mew=1.0, label='NPEO 2001-2005')
+#    proj.plot(x_cord['HLY']  , y_cord['HLY']  , 'k+', ms=8, mew=2.5, label='HLY0301 2003')
+#    proj.plot(x_cord['CBL']  , y_cord['CBL']  , 'k<', ms=6, mew=0.5, label='CBL 2002')
+#    proj.plot(x_cord['ARK14'], y_cord['ARK14'], 'k>', ms=6, mew=0.5, label='ARKTISXIV-II 1998')
+#    proj.plot(x_cord['ARC']  , y_cord['ARC']  , 'kv', ms=6, mew=0.5, label='ARCSS107-1993')
+#    #proj.plot(gridx.T, gridy.T, 'k-', lw=0.25)
+#    #proj.plot(gridx, gridy, 'k-', lw=0.25)
+#    cbaxes = fig.add_axes([0.975, 0.15, 0.02, 0.5]) 
+#    CBar = plt.colorbar(CS, ax=ax, cax=cbaxes) 
+#    CBar.set_label('Interpolated diss.Ba con. ( 1E-6 mol/L )', fontsize=12)
+#    CBar.ax.tick_params(axis='y', length=0)
+#    LG = ax.legend(numpoints=1, bbox_to_anchor=(0.95, 1), loc=2, borderaxespad=0.); LG.draw_frame(False)
+#    ax.set_title('Initial field in a regular lat/lon frame, depth='+str(deptht[num])+'m', fontsize=15, y=1.025)
+#    fig.savefig('Initial_field'+str(num)+'.pdf', dpi=600, orientation='portrait', papertype='a4', format='pdf',
+#                transparent=False, bbox_inches='tight', pad_inches=0)
 
 # Save
-save_var = { 'Ba_ini_xy': Ba_ini_xy, 'nav_lon': nav_lon, 'nav_lat': nav_lat}
-scipy.io.savemat('_data/Temp_Ba_int_ANHA4.mat', mdict=save_var)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+save_var = { 'Ba_ini_xy': Ba_ini_xy, 'lon': lonxy, 'lat': latxy}
+scipy.io.savemat('../_data/Exchange/Temp_Ba_int_ANHA4.mat', mdict=save_var)
 
